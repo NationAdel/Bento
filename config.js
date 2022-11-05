37f50190e2f7721bab19f05307a0d528b86fbd71
NationAdel <111717429+NationAdel@users.noreply.github.com>
2022-11-05 19:57:51 -0400

    Update config.js
    Signed-off-by: NationAdel <111717429+NationAdel@users.noreply.github.com>

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+	weatherKey: '0cd252b4045d6cd857300d04be784a98', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
